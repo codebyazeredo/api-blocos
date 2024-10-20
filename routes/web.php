d812ecc8769fcaa787657aee4e914bb021826da3
codebyazeredo <matheus.azeredo@hotmail.com>
2024-10-20 12:10:03 -0300

    Feat: Pedidos Routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\PedidoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('pedidos')->group(function () {
+    Route::get('/', [PedidoController::class, 'index']);
+    Route::post('/', [PedidoController::class, 'store']);
+    Route::get('/{id}', [PedidoController::class, 'show']);
+    Route::put('/{id}', [PedidoController::class, 'update']);
+    Route::delete('/{id}', [PedidoController::class, 'destroy']);
 });
